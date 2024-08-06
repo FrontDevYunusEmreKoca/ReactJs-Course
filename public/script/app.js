da34fedd19a2df955e777cab1e12a0362ff61249
Yunus Emre KOCA <yunsemrekoca@gmail.com>
2024-08-06 12:42:14 +0300

    reactda 3 cesít kosullu ifadeler 1-if else 2-function 3- ? operatoru

--- public/script/app.js
+++ public/script/app.js
@@ -6,15 +6,31 @@ var root = document.getElementById("root");
 var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
   id: "header"
 }, "Hello World"), /*#__PURE__*/React.createElement("div", null, "Lorem ipsum dolor sit amet."), /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum dolor."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum dolor."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum dolor."), /*#__PURE__*/React.createElement("li", null, "Lorem, ipsum dolor.")));
+
+// var name = "Samsung S10";
+// var price = 5000;
+// var description = "cok iyi bir telefon";
+
+var product = {
+  name: "Samsung S10",
+  price: 7700,
+  description: "Cok iyi bir telefon",
+  types: ["red", "blue", "orange"]
+};
+function getDescription(description) {
+  if (description) {
+    return /*#__PURE__*/React.createElement("p", {
+      id: "product-des"
+    }, "description: ", description);
+  } else {
+    return "no des";
+  }
+}
 var template2 = /*#__PURE__*/React.createElement("div", {
   id: "product-details"
 }, /*#__PURE__*/React.createElement("h2", {
   id: " product-name"
-}, "Samsung S9"), /*#__PURE__*/React.createElement("p", {
-  id: "product-price"
-}, "price: 3000 TL"), /*#__PURE__*/React.createElement("p", {
-  id: "product-des"
-}, "description: iyi bir telefon"));
+}, "name: ", product.name ? product.name : "no name"), product.price && product.price > 0 && /*#__PURE__*/React.createElement("p", null, "price:", product.price, " TL"), getDescription(product.description), /*#__PURE__*/React.createElement("p", null, "types: ", product.types.length > 0 ? "there are optains" : "no optains"));
 
 //reactDom
 ReactDOM.render(template2, root);
--- src/app.js
+++ src/app.js
@@ -13,11 +13,44 @@ var template = <div>
      </ul>
 </div>;
 
-var template2 = (<div  id="product-details">
-    <h2 id =" product-name">Samsung S9</h2>
-    <p id="product-price" >price: 3000 TL</p>
-    <p  id="product-des" >description: iyi bir telefon</p>
-</div>);
+// var name = "Samsung S10";
+// var price = 5000;
+// var description = "cok iyi bir telefon";
+
+var product = {
+        name : "Samsung S10",
+        price : 7700,
+        description: "Cok iyi bir telefon",
+        types :
+        [
+            "red",
+            "blue",
+            "orange"
+        ]
+       
+}
+function getDescription(description) {
+    if (description)
+    {
+        return <p id="product-des">description: {description}</p> 
+    }
+    else 
+    {
+        return "no des";
+    }
+   
+
+}
+
+
+var template2 = (
+    <div  id="product-details">
+        <h2 id =" product-name">name: {product.name ? product.name: "no name"}</h2>    
+        {(product.price && product.price > 0 ) &&  <p>price:{product.price} TL</p> }
+        {getDescription(product.description)}
+        <p>types: {product.types.length > 0 ?  "there are optains" : "no optains" }</p>
+    </div>
+);
 
 
 
