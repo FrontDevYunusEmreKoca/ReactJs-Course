b0689f70347114f9b6312c29e7e28b448bd12478
Yunus Emre KOCA <yunsemrekoca@gmail.com>
2024-08-06 14:43:42 +0300

    const -let scope

--- src/examples/const-let.js
+++ src/examples/const-let.js
@@ -1,12 +1,12 @@
-"use strict";
-
 // var nameVar = "Yunus";
 // var nameVar = "emre"
 // console.log(nameVar)
 
+
 // let nameLet = "yunus"
 // let nameLet = "emre";
 // console.log(nameLet)
+
 
 // const nameCost = "emre";
 
@@ -22,8 +22,9 @@
 // console.log(name) // bu gelmez cunku functionun icinde
 // getAge();
 
-var city = "Kocaeli";
-if (true) {
-  city = "IStanbul";
-  console.log(city);
+
+var city =  "Kocaeli";
+if (true){
+    city = "IStanbul"
+    console.log(city)
 }
