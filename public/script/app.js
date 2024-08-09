d3654f931cfc5f9e85b54c4bd8b543ac5731ca13
Yunus Emre KOCA <yunsemrekoca@gmail.com>
2024-08-09 10:54:09 +0300

    this

--- public/script/app.js
+++ public/script/app.js
@@ -1,45 +1,25 @@
 "use strict";
 
-// JSX - JS Xml
-
-var root = document.getElementById("root");
-var app = {
-  title: "To Do APP",
-  description: "lorem ipsum dolor",
-  items: ["item1", "item2", "item3"]
-};
-var onFormSubmit = function onFormSubmit(event) {
-  event.preventDefault(); // yeni bir tane event olusturdugumuzda sayfa yenilenmesin istiyorum
-  var item = event.target.elements.txtItem.value;
-  if (item) {
-    app.items.push(item);
-    event.target.elements.txtItem.value = "";
-    render();
+var user = {
+  name: "YunusEmre",
+  email: "kocay1857@gmail.com",
+  city: "Kocaeli",
+  roles: ["admin", "customer"],
+  getRoles: function getRoles() {
+    var _this = this;
+    this.roles.forEach(function (role) {
+      console.log(role);
+      console.log(_this.name);
+    });
   }
-  console.log("form submitted");
 };
-var clearItems = function clearItems() {
-  app.items = [];
-  render();
+user.getRoles();
+var addes5 = function addes5() {
+  console.log(arguments); // kac adeet parametre gonderecegimi bilmiyorsam arguments olarak yazarim
+  var total = 0;
+  for (var i = 0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+  return total;
 };
-var render = function render() {
-  var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
-    id: "header"
-  }, app.title), /*#__PURE__*/React.createElement("div", null, app.description, "."), /*#__PURE__*/React.createElement("ul", null, app.items.map(function (item, index) {
-    // numbersdan number dizisi olustur // map metodu
-    return /*#__PURE__*/React.createElement("li", {
-      key: index
-    }, item);
-  })), /*#__PURE__*/React.createElement("p", null, /*#__PURE__*/React.createElement("button", {
-    onClick: clearItems
-  }, "Clear Items")), /*#__PURE__*/React.createElement("p", null, app.items.length), /*#__PURE__*/React.createElement("form", {
-    onSubmit: onFormSubmit
-  }, /*#__PURE__*/React.createElement("input", {
-    type: "text",
-    name: "txtItem"
-  }), /*#__PURE__*/React.createElement("button", {
-    type: "submit"
-  }, "Add Item")));
-  ReactDOM.render(template, root);
-};
-render();
+console.log(addes5(5, 10, 15, 20, 25));
