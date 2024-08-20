113e0bf3c6bb5699e07d890dd87b2896154b19f0
Yunus Emre KOCA <yunsemrekoca@gmail.com>
2024-08-20 10:53:16 +0300

    clear results responseleri temizle

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
   constructor(props){
     super(props)
     this.searchUsers = this.searchUsers.bind(this)
+    this.clearUsers = this.clearUsers.bind(this)
     this.state = {
       loading:false,
       users:[]
@@ -21,6 +22,7 @@ class App extends React.Component {
       axios
       .get("https://api.github.com/users")
       .then(res => this.setState({users:res.data, loading:false}))
+     
     },1000)
    
    }
@@ -30,14 +32,19 @@ class App extends React.Component {
       axios
       .get(`https://api.github.com/search/users?q=${keyword}`)
       .then(res => this.setState({users:res.data.items, loading:false}))
+    
     },1000)
-   
+  }
+  clearUsers (){
+    this.setState({
+        users:[]
+    })
   }
    render(){
     return (
       <React.Fragment>
         <Navbar />
-        <Search searchUsers={this.searchUsers} /> 
+        <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} /> 
         <Users users = {this.state.users} loading = {this.state.loading} />
         
       </React.Fragment>    // react Fragmentin divden farki alan olsuturmaz ama div gorevi gorur
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,41 +1,53 @@
-import React from 'react'
+import React from "react";
 
-class Search extends React.Component{
-    constructor (props){
-        super(props)
-        this.onChange=this.onChange.bind(this)
-        this.onSubmit=this.onSubmit.bind(this)
-       
-        this.state = {
-           keyword : ""
-        }
-    }
-    onChange(e){
-      
-        this.setState({
-            keyword:e.target.value
-        })
-    }
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      keyword: "",
+    };
+  }
+  onChange(e) {
+    this.setState({
+      keyword: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    this.props.searchUsers(this.state.keyword);
+ 
+    this.setState({
+      keyword: "",
+    });
+  }
 
 
-    onSubmit(e){
-        e.preventDefault();
-        this.props.searchUsers(this.state.keyword)
-    }
-    render(){
-        return (
-            <form className='' onSubmit={this.onSubmit}>
-                <div className="container my-4">
-                <div className='input-group'>
-                <input type="text" onChange={this.onChange} className="form-control"/>
-                <div className='input-group-append'>
-                    <button type='submit' className=' btn btn-primary'>Search</button>
-                </div>
-                </div>
-                </div>
-                
-            </form>
-        )
-    }
+  render() {
+    return (
+      <div className="container my-4">
+        <form className="" onSubmit={this.onSubmit}>
+          <div className="input-group">
+            <input
+              value={this.state.keyword}
+              id="searchId"
+              type="text"
+              onChange={this.onChange}
+              className="form-control"
+            />
+            <div className="input-group-append">
+              <button type="submit" className=" btn btn-primary">
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
+        <button  onClick={this.props.clearUsers} className="btn btn-secondary btn-sm w-100 my-3">Clear Results</button>
+      </div>
+    );
+  }
 }
-export default Search
+export default Search;
